7f541a0552ae0d40fc9ad8a9f80988e9168b7392
Nandini Gupta <nandanigupta1132003@gmail.com>
2022-07-31 16:34:42 +0530

    validations and api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Login from './login/login';
+import Login from './login/Login';
 function App() {
   return (
     <div className="App">
--- src/login/login.js
+++ src/login/login.js
@@ -1,11 +1,81 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from "react";
+import { useState } from "react";
+import React from 'react'
 import "./login.css";
 import "antd/dist/antd.css";
 import Loginimg from "../Images/loginImg.png";
 import { UserOutlined } from "@ant-design/icons";
+// import { toast } from "react-toastify";
+import axios from "axios";
 import { Input, Space, Button, Checkbox } from "antd";
-const login = () => {
+const Login = () => {
+
+  const [email,setEmail] = useState("");
+  const [psswd,setPsswd] = useState(""); 
+  const [emailerror,setEmailerror] = useState("");
+  const [psswderror,setpsswderror] = useState("");
+  const [emailerrortype,setEmailerrortype] = useState("");
+  const [psswderrortype,setPsswderrortype] = useState("");
+  const data = {
+    email : email,
+    password: psswd
+  }
+  
+  const handlelogin = (e) =>{
+    e.preventDefault();
+    setEmailerrortype(validateEmail(email));
+    setPsswderrortype(validatePassword(psswd));
+    if(emailerrortype || psswderrortype){
+      console.log("err");
+    }
+    else{
+      axios.post("https://reqres.in/api/login",data)
+      .then((res) => {
+        console.log(res.data);
+        if(res.data.token){
+          // toast.error("Select any language first");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    }
+  }
+
+
+  
+  const validateEmail= (value) => {
+    let error;
+    const regex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!value) {
+      error = "Email Required";
+      setEmailerror(true);
+    } else if (!regex.test(value)) {
+      error = "Invalid EmailId";
+      setEmailerror(true);
+    } else {
+      setEmailerror(false);
+    }
+    return error;
+  };
+
+  const validatePassword = (value) => {
+    let error;
+    // const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+    if (!value) {
+      error = "Password Required";
+      setpsswderror(true);
+    }
+    //  else if (!regex.test(value)) {
+    //   error = "Password Invalid";
+    //   setpsswderror(true);
+    // } 
+    else {
+      setpsswderror(false);
+    }
+    return error;
+  };
+
   return (
     <div className="Login">
       <div className="box1">
@@ -15,19 +85,30 @@ const login = () => {
             <h2>Sub-title text goes here</h2>
           </div>
           <div className="inputs">
+          <span className="error">{emailerrortype}</span>
             <Space direction="vertical" className="inputval">
               <Input
-                style={{"border-radius":"0.2rem"}}
+                status = {(emailerror === true)? "error" : " "}
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ "bordeRadius": "0.2rem" }}
                 placeholder="Email Address *"
                 prefix={<UserOutlined className="site-form-item-icon" />}
               />
             </Space>
+            <span className="error">{psswderrortype}</span>
             <Space direction="vertical" className="inputval">
-              <Input.Password  style={{"border-radius":"0.2rem"}}placeholder="Password *" />
+              <Input.Password
+                status = {(psswderror === true)? "error" : ""}
+                onChange={(e) => setPsswd(e.target.value)}
+                style={{ "borderRadius": "0.2rem" }}
+                placeholder="Password *"
+              />
             </Space>
-            <Button>Login</Button>
+            <Button onClick={(e) => handlelogin(e)}>Login</Button>
             <div className="lowerline">
-              <Checkbox><h4>Remember Password</h4></Checkbox>
+              <Checkbox>
+                <h4>Remember Password</h4>
+              </Checkbox>
               <p href="">Forgot Password?</p>
             </div>
           </div>
@@ -40,4 +121,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
